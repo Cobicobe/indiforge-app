2736100ead1f54a7febccd8ecba7d5cc7944cdbc
Cobicobe <jdanforthco3@gmail.com>
2025-05-05 02:43:06 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -3,12 +3,12 @@ import DatasetUpload from './components/DatasetUpload';
 
 function App() {
   return (
-    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
+    <div>
       <h1>IndiForge.AI</h1>
-      <p>Upload your dataset to register it securely on Solana.</p>
       <DatasetUpload />
     </div>
   );
 }
 
 export default App;
+
