f9cc8ca07b1e6abb69aabf66028b687e2dc12f7b
Cobicobe <jdanforthco3@gmail.com>
2025-05-05 01:59:55 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import WalletConnect from './components/WalletConnect';
 import DatasetUpload from './components/DatasetUpload';
-import DatasetCreator from './components/DatasetCreator';
-
 
 function App() {
-  const handleDatasetReady = ({ title, description, hash }) => {
-    console.log('Dataset Ready:', title, description, hash);
-    // Later: send this info to Solana backend for licensing
-  };
-
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
       <h1>IndiForge.AI</h1>
-      <WalletConnect />
-      <DatasetCreator onDatasetReady={handleDatasetReady} />
+      <p>Upload your dataset to register it securely on Solana.</p>
       <DatasetUpload />
     </div>
   );
