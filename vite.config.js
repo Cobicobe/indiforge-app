331b231ff030fd3733c4dcc8d26d9b929fb83342
Cobicobe <jdanforthco3@gmail.com>
2025-05-05 02:16:15 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/indiforge-app/' // needed for GitHub Pages deployment
+  base: '/indiforge-app/', // GitHub Pages path
 });
